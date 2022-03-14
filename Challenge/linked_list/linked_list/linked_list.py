e2324aaa595663a5368153eb21707f612ec9d51c
joud-alnsour <alnsour.joud@gmail.com>
2022-03-14 23:01:58 +0300

    linked

--- Challenge/linked_list/linked_list/linked_list.py
+++ Challenge/linked_list/linked_list/linked_list.py
@@ -52,13 +52,68 @@ class LinkedList:
             
     def to_string(self):
         return self.__str__()
+
+    def append(self,value):
+        if value == '':
+            raise TypeError('Node not empty')
+        else:
             
-     
+            if self.head is None:
+                self.head = value
+            
+            else:
+                current = self.head
+                while current.next is not None:
+                    current = current.next
+                current.next = value   
+
+
+    def insert_after(self, value , new):
+
+        if self.head == None:
+            return
+
+        current =  self.head
+        while current:
+            if current.value == value:
+                current.next=Node(new , current.next)
+            current = current.next
+        return
+
+       
+
+    def insert_befor(self, value , new):
+         
+        if self.head == None:
+            return
+
+        if self.head.value == value:
+            self.insert_at_beginning()
+            return
+
+        current =  self.head
+        while current:
+            if self.head == None:
+                break
+
+            if current.next.value == value: 
+                node = Node(new , current.next)
+                node.next= current.next
+                current.next = node    
+                return
+            else:
+                return (-1)
+
+
+
 if __name__ == '__main__':
    ll=LinkedList()
    ll.head=Node('joud')
    ll.x=Node('good')
    ll.insert(Node(True)) 
+   ll.insert_after('add' , 'nice')
+   ll.insert_befor('python' , 'language')
    print(ll.includes('joud'))
+   #ll = ll.__str__()
    print(ll)
   
--- Challenge/linked_list/tests/test_linked_list.py
+++ Challenge/linked_list/tests/test_linked_list.py
@@ -6,12 +6,12 @@ def test_empty_linked():
     excepted= None
     actual== excepted
 
-def test_add():
-    add= LinkedList()
-    add.insert(Node('joud'))
-    add.insert(Node('hello'))
-    add.insert(Node('hi'))
-    assert 'hi ->hello ->joud ->NULL'== add.to_string()
+def test_python():
+    python= LinkedList()
+    python.insert(Node('joud'))
+    python.insert(Node('hello'))
+    python.insert(Node('hi'))
+    assert '"hi ->hello ->joud ->NULL"'== python.to_string()
 
 
 def test_includes():
@@ -19,5 +19,36 @@ def test_includes():
     listt.insert(Node('good'))
     assert listt.includes('good')==True
     assert listt.includes(9)==False
+
+
+def test_insert_after():
+    ll = LinkedList()
+    
+    ll.insert_at_beginning('python')
+    ll.insert_at_beginning('add')
+    ll.insert_at_beginning('joud')
+    ll.insert_after('add' , 'nice')
+    actual = ll.__str__()
+    expected = '"{joud}-> {add}-> {nice}-> {python}-> NULL"'
+    assert actual == expected
+
+def test_insert_befor():
+    ll = LinkedList()
+    ll.insert_at_beginning('python')
+    ll.insert_at_beginning('add')
+    ll.insert_at_beginning('joud')
+    ll.insert_befor('python' , 'language')
+    actual = ll.__str__()
+    expected ='"{joud}-> {add}-> {language}-> {python}-> NULL"'
+    assert actual == expected  
+
+
+def test_append1():
+    ll = LinkedList()
+    ll.insert_values(['list' , 'python' ])
+    ll.append('fix')
+    actual =ll.__str__()
+    expected = '"{list}-> {python}-> {fix}-> NULL"'
+    assert actual == expected      
 
 
